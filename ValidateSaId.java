fef6f99ef2de2c0aad06d8633cfff2cc34e04e06
LindiweD-Collab <lindiwethaby96@gmail.com>
2024-10-15 21:28:42 +0200

    Update and rename Id_Validator.Java to ValidateSaId.java

--- ValidateSaId.java
+++ ValidateSaId.java
@@ -1,18 +1,15 @@
-// ValidateSaId.java
 public class ValidateSaId {
     public static boolean isIdNumberValid(String idNumber) {
-    
+        
         if (idNumber.length() != 13) return false;
 
-        
         if (!idNumber.matches("\\d+")) return false;
 
-       
+        
         int year = Integer.parseInt(idNumber.substring(0, 2));
         int month = Integer.parseInt(idNumber.substring(2, 4));
         int day = Integer.parseInt(idNumber.substring(4, 6));
 
-        if (year < 0 || year > 99) return false;
         if (month < 1 || month > 12) return false;
         if (day < 1 || day > 31) return false;
 
@@ -26,16 +23,19 @@ public class ValidateSaId {
 
        
         int sum = 0;
-        for (int i = 0; i < 13; i++) {
-            int digit = Integer.parseInt(String.valueOf(idNumber.charAt(i)));
-            if (i % 2 == 0) {
-                sum += digit;
-            } else {
-                sum += digit * 2 > 9 ? digit * 2 - 9 : digit * 2;
+        boolean alternate = false;
+        for (int i = idNumber.length() - 1; i >= 0; i--) {
+            int n = Integer.parseInt(idNumber.substring(i, i + 1));
+            if (alternate) {
+                n *= 2;
+                if (n > 9) {
+                    n = (n % 10) + 1;
+                }
             }
+            sum += n;
+            alternate = !alternate;
         }
-        if (sum % 10 != 0) return false;
-
-        return true;
+        return (sum % 10 == 0);
     }
 }
+
